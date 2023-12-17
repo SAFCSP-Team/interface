717685db4a34567217028a0874f67bcc9dff4878
devAlq <khalidalqdev@gmail.com>
2023-12-17 23:20:20 +0300

    taskDone

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
-public class Main {
+// * Create an interface named `Shape` which should contain only one method called `calculateArea()`.
+// * Create `Rectangle` and `Circle` classes that implement the `Shape` interface.
+// * Override the `calculateArea()` method in each class.
+// * In the main method, create an object from each class and return the result of `calculateArea()` method, and print it.
+
+
+public class Main implements Shape {
+    private double circleResult;
+    private float rectangleResult;
+
+         public double Circle(double radius){
+            this.circleResult = radius * 3.14;
+            return this.circleResult;
+
+        }
+
+        
+
+
+        public float Rectangle(float length, float width){
+            this.rectangleResult = length * width;
+            return this.rectangleResult;
+        }
+
+        public void printCircle(){
+            System.err.println(this.circleResult);
+        }
+        
+         public void rectangleResult(){
+            System.err.println(this.rectangleResult);
+        }
+        
     public static void main(String[] args) {
-       /* write your code here */
+       Main circle = new Main();
+       circle.Circle(3);
+       circle.printCircle();
+       Main rect = new Main();
+       rect.Rectangle(6 , 4);
+       rect.rectangleResult();
     }
 }
